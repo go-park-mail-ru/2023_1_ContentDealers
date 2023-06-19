f12c23d602a70ae43c39efe42d681c9154de3eb1
Ivan Bulgakov <wanjasoldier@gmail.com>
2023-06-19 04:06:32 +0300

    индексы для поиска.

--- scripts/create_db.sql
+++ scripts/create_db.sql
@@ -24,7 +24,7 @@ create type content_type as enum (
 
 -- tables
 
--- 2 нормальная форма
+-- 3 нормальная форма
 create table user_schema.users (
     id bigserial primary key,
     email text not null unique, -- пользователя идентифицируем по email
@@ -35,7 +35,7 @@ create table user_schema.users (
     updated_at timestamp not null default now()
 );
 
--- 6 нормальная форма
+-- 3 нормальная форма
 create table user_action_schema.users_content_favorites (
     user_id bigint not null,
     content_id bigint not null,
@@ -43,7 +43,7 @@ create table user_action_schema.users_content_favorites (
     primary key (user_id, content_id)
 );
 
--- 6 нормальная форма
+-- 3 нормальная форма
 create table user_action_schema.users_persons_favorites (
     user_id bigint not null,
     person_id bigint not null,
@@ -51,7 +51,7 @@ create table user_action_schema.users_persons_favorites (
     primary key (user_id, person_id)
 );
 
--- 6 нормальная форма
+-- 3 нормальная форма
 create table user_action_schema.ratings (
     user_id bigint not null,
     content_id bigint not null,
@@ -60,7 +60,7 @@ create table user_action_schema.ratings (
     primary key (user_id, content_id)
 );
 
--- доменно-ключевая нормальная форма
+-- 3 нормальная форма
 create table user_action_schema.history_views (
     user_id bigint not null,
     content_id bigint not null,
@@ -70,13 +70,13 @@ create table user_action_schema.history_views (
     primary key (user_id, content_id)
 );
 
--- 6 нормальная форма
+-- 3 нормальная форма
 create table content_schema.roles (
     id bigserial primary key,
     title text unique not null -- Одна запись - одна роль
 );
 
--- 5 нормальная форма
+-- 3 нормальная форма
 create table content_schema.persons (
     id bigserial primary key,
     name text not null,
@@ -87,7 +87,7 @@ create table content_schema.persons (
     age integer
 );
 
--- 5 нормальная форма
+-- 3 нормальная форма
 create table content_schema.content (
     id bigserial primary key,
     title text not null,
@@ -104,27 +104,27 @@ create table content_schema.content (
     count_ratings bigint not null default 0
 );
 
--- 2 нормальная форма
+-- 3 нормальная форма
 create table content_schema.films (
     id bigserial primary key,
     content_id bigint not null references content(id) on delete cascade,
     content_url text not null
 );
 
--- 6 нормальная форма
+-- 3 нормальная форма
 create table content_schema.selections (
     id bigserial primary key,
     title text not null
 );
 
--- 6 нормальная форма
+-- 3 нормальная форма
 create table content_schema.content_selections (
     content_id bigint references content(id) on delete cascade,
     selection_id bigint references selections(id) on delete cascade,
     primary key (content_id, selection_id)
 );
 
--- 6 нормальная форма
+-- 3 нормальная форма
 create table content_schema.content_roles_persons (
     role_id bigint references roles(id) on delete cascade,
     person_id bigint references persons(id) on delete cascade,
@@ -132,39 +132,39 @@ create table content_schema.content_roles_persons (
     primary key (role_id, person_id, content_id)
 );
 
--- 6 нормальная форма
+-- 3 нормальная форма
 create table content_schema.countries (
     id bigserial primary key,
     name text not null
 );
 
--- 6 нормальная форма
+-- 3 нормальная форма
 create table content_schema.genres (
     id bigserial primary key,
     name text not null
 );
 
--- 6 нормальная форма
+-- 3 нормальная форма
 create table content_schema.content_countries (
     content_id bigint references content(id) on delete cascade,
     country_id bigint references countries(id) on delete cascade,
     primary key (content_id, country_id)
 );
 
--- 6 нормальная форма
+-- 3 нормальная форма
 create table content_schema.content_genres (
     content_id bigint references content(id) on delete cascade,
     genre_id bigint references genres(id) on delete cascade,
     primary key (content_id, genre_id)
 );
 
--- 6 нормальная форма
+-- 3 нормальная форма
 create table content_schema.series (
     id bigserial primary key,
     content_id bigint not null references content(id) on delete cascade
 );
 
--- 5 нормальная форма
+-- 3 нормальная форма
 create table content_schema.episodes (
     id bigserial primary key,
     series_id bigint not null references series(id) on delete cascade,
@@ -188,6 +188,9 @@ create index if not exists episodes__series_id on content_schema.episodes(series
 create index if not exists films__content_id on content_schema.films(content_id);
 create index if not exists series__content_id on content_schema.series(content_id);
 
+-- search indexes
+create index if not exists content__title on content_schema.content using gin (title public.gin_trgm_ops);
+create index if not exists persons__name on content_schema.persons using gin (name public.gin_trgm_ops);
 
 -- triggers and functions
 
@@ -218,5 +221,3 @@ create trigger update_rating_trigger
 before update of sum_ratings, count_ratings on content_schema.content
 for each row
 execute function content_schema.update_rating();
-
-
